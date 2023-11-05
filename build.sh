0f5decee9f5b9f84760159cf133b43ef1493ed6a
tamanegi-mogumogu <nyusuke2008@gmail.com>
2023-11-05 17:06:46 +0900

    Use treble patch

---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo
+echo "--------------------------------------"
+echo "          Evolution X 14.0 Buildbo    "
+echo "                  by                  "
+echo "              boydaihungs             "
+echo "         Origin author: ponces        "
+echo "--------------------------------------"
+echo
+
+set -e
+
+GIT_REPO="treble_patch"
+GIT_OWNER="tqmane"
+
+BL="$PWD/$GIT_REPO"
+OUT="out/target/product/tdgsi_arm64_ab"
+BD="$PWD/GSIs"
+EVO_BASE_VERSION="8"
+
+buildDate="$(date +%Y%m%d)"
+version="$(date +v%Y.%m.%d)"
+
+START=$(date +%s)
+timestamp="$START"
+
+applyPatches() {
+    echo "--> Applying patches"
+    bash $BL/apply-patches.sh .
+    echo
+
+    echo "--> Generating makefiles"
+    cp $BL/evo.mk ./device/phh/treble/
+    pushd ./device/phh/treble/ &>/dev/null
+      bash generate.sh evo
+    popd &>/dev/null
+    echo
+}
+
+applyPatches
+END=$(date +%s)
+ELAPSEDM=$(($(($END-$START))/60))
+ELAPSEDS=$(($(($END-$START))-$ELAPSEDM*60))
+
+echo "--> Buildbot completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
+echo
