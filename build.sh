002c7396d761d383d3aba88b747e6f0d7dedbfff
tqmane <149690876+tqmane@users.noreply.github.com>
2023-11-07 22:39:36 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,6 +14,15 @@ set -e
 GIT_REPO="treble_patch"
 GIT_OWNER="tqmane"
 
+BL="$PWD/$GIT_REPO"
+BD="$PWD/GSIs"
+
+buildDate="$(date +%Y%m%d)"
+version="$(date +v%Y.%m.%d)"
+
+START=$(date +%s)
+timestamp="$START"
+
 applyPatches() {
     echo "--> Applying patches"
     bash $BL/apply-patches.sh .
